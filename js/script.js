3bb46ad78109a3a4c91086512828c6e3a96d09ad
lil-melon <133168636+lil-melon@users.noreply.github.com>
2024-09-19 23:03:30 -0700

    init files

---
+++ js/script.js
@@ -0,0 +1,207 @@
+// STEP 1
+
+/*
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+*/
+
+// STEP 2
+
+/*
+//numeric literal expression
+24
+
+//string literal expression
+"Tree"
+
+//boolean literal expression
+false
+
+//null literal expression
+null
+*/
+
+// STEP 3
+
+/*
+let fruit = "apple"
+let numSteps = 10000
+*/
+
+// STEP 4
+
+/*
+let strFirstName
+let strLastName
+let strAdress
+let strCity
+let strState
+let strZipCode //since a zip code can start with a 0
+let intAge
+let strReferralSource
+let boolContactPermission
+*/
+
+// STEP 5
+
+/*
+let strFirstName
+let strLastName 
+let strState
+
+let strFirstName = prompt("What is your first name?")
+let strLastName  = prompt("What is your last name?")
+let strState     = prompt("What state are you from?")
+
+strFirstName = "Bob", strLastName = true, strState = "CA"
+*/
+
+// STEP 6
+
+/*
+let test
+test = 10 + " is a number."
+console.log(test)
+*/
+
+// STEP 7
+
+/*
+let test1
+let test2
+
+test1 = false + " is not true"
+console.log(test1)
+
+test2 = 2 + true
+console.log(test2)
+*/
+
+// STEP 8
+
+/*
+//It is not valid because the apostrophe is not escaped.
+//To fix, you would escape the apostrophe like this:
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+*/
+
+// STEP 9
+
+/*
+//for null
+let a = null
+console.log(a)
+
+//for undefined
+let b
+console.log(b)
+*/
+
+// STEP 10
+
+/*
+typeof "Hello"
+typeof 3
+typeof true
+typeof {"tree":"pine"}
+typeof c
+*/
+
+// STEP 11
+
+/*
+alert("Hello " + "Ellen Noh" + ", welcome to the JavaScript class!")
+*/
+
+// STEP 12
+
+/*
+let name = "Ellen Noh"
+alert("Hello " + name + ", welcome to the JavaScript class!")
+*/
+
+// STEP 13
+
+/*
+let name = "Ellen Noh"
+let course = "JavaScript"
+alert("Hello " + name + ", welcome to the "+course+" class!")
+*/
+
+// STEP 14
+
+/*
+let name = "Ellen Noh"
+let course = "JavaScript"
+alert("Hello " + name + ".\nWelcome to the "+course+" class!")
+*/
+
+// STEP 15
+
+/*
+let name = prompt("What is your name?")
+let course = "JavaScript"
+alert("Hello " + name + ".\nWelcome to the "+course+" class!")
+*/
+
+// STEP 16
+
+/*
+let name = prompt("What is your name?")
+let course = prompt("What class are you taking?")
+alert("Hello " + name + ".\nWelcome to the "+course+" class!")
+*/
+
+// STEP 17
+
+/*
+let x = 10
+let y = 20
+console.log(x+y)
+*/
+
+// STEP 18
+
+/*
+let x = 20
+x += 20
+console.log(x)
+*/
+
+// STEP 19
+
+/*
+let x = 20
+x *= 5
+console.log(x)
+*/
+
+// STEP 20
+
+/*
+x = 20%3
+x /= 1
+console.log(x)
+*/
+
+// STEP 21
+
+/*
+let isSunnyDay = true
+let temp = 73
+let isBeautifulDay = isSunnyDay && temp < 75
+console.log(isBeautifulDay)
+*/
+
+//STEP 22
+
+/*
+let hadFood = false
+let hoursSlept = 4
+let inGoodMood = hadFood || hoursSlept > 7
+console.log(inGoodMood)
+*/
